424b700d290788e4fefe0c3de4273c18e48d33c6
Octavio Ignativ <egeye4y@gmail.com>
2017-04-24 14:15:19 +0800

    Create DisplayStyle.js

---
+++ JS/DisplayStyle.js
@@ -0,0 +1,18 @@
+/**
+ * [获取标签id，来改变该style]
+ * @param  {[type]}  [description]
+ * @return {[type]}  [description]
+ */
+document.getElementById('elemId').style.display = 'none';
+$('#elemId').removeAttr('style');
+	
+if(testFlag) 
+{
+	$('#elemId').css('display','none');
+	testFlag = false;
+}
+else
+{
+	$('#elemId').css('display','block');
+	testFlag = true;
+}
