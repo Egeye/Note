5901cc61157321ed45608e8228cfba6b8d6e2820
Octavio <egeye4y@163.com>
2017-05-21 22:28:12 +0800

    Optimize view

--- JavaScript/AngularJS/js/index.js
+++ JavaScript/AngularJS/js/index.js
@@ -66,7 +66,64 @@ app4.filter('myFormat',['hexafy4', function(hexafy4) {
 var app6 = angular.module('app6', []);
 app6.controller('controllerApp6', function($scope) {
     $scope.app6Names = ["Google", "Runoob", "Taobao"];
+    $scope.btnReset = function() {
+    	$scope.count = 0;
+    }
 });
+
+
+// ----------------------Start------------------------------------
+angular.module('myApp', []).controller('userCtrl', function($scope) {
+	$scope.fName = '';
+	$scope.lName = '';
+	$scope.passw1 = '';
+	$scope.passw2 = '';
+	$scope.users = [
+		{id:1, fName:'Hege', lName:"Pege" },
+		{id:2, fName:'Kim',  lName:"Pim" },
+		{id:3, fName:'Sal',  lName:"Smith" },
+		{id:4, fName:'Jack', lName:"Jones" },
+		{id:5, fName:'John', lName:"Doe" },
+		{id:6, fName:'Peter',lName:"Pan" }
+	];
+	$scope.edit = true;
+	$scope.error = false;
+	$scope.incomplete = false;
+
+	$scope.editUser = function(id) {
+		if (id == 'new') {
+		    $scope.edit = true;
+		    $scope.incomplete = true;
+		    $scope.fName = '';
+		    $scope.lName = '';
+	    } else {
+		    $scope.edit = false;
+		    $scope.fName = $scope.users[id-1].fName;
+		    $scope.lName = $scope.users[id-1].lName;
+		}
+	};
+
+	$scope.$watch('passw1',function() {$scope.test();});
+	$scope.$watch('passw2',function() {$scope.test();});
+	$scope.$watch('fName', function() {$scope.test();});
+	$scope.$watch('lName', function() {$scope.test();});
+
+	$scope.test = function() {
+		if ($scope.passw1 !== $scope.passw2) {
+	    	$scope.error = true;
+	    } else {
+	    	$scope.error = false;
+		}
+		$scope.incomplete = false;
+		if ($scope.edit && (!$scope.fName.length ||
+			!$scope.lName.length ||
+			!$scope.passw1.length || !$scope.passw2.length)) {
+			$scope.incomplete = true;
+		}
+	};
+
+});
+// ----------------------End------------------------------------
 
 angular.bootstrap(document.getElementById("app1"), ['myApp1']);
 angular.bootstrap(document.getElementById("app2", ['myApp2']));
@@ -76,3 +133,4 @@ angular.bootstrap(document.getElementById("appId3"), ['app3']);
 angular.bootstrap(document.getElementById("app4Id"), ['app4']);
 // angular.bootstrap(document.getElementById('app5id'), ['app5']);
 angular.bootstrap(document.getElementById('idApp6'), ['app6']);
+angular.bootstrap(document.getElementById('idApp7'), ['myApp']);
