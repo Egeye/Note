0d9f23fd91f72299d131f467e2d2b06e074a2baf
Octavio Ignativ <egeye4y@gmail.com>
2017-07-21 10:36:36 +0800

    Create controller.js

---
+++ Code/angular-file-upload-springMVC/controller.js
@@ -0,0 +1,51 @@
+    // 使用插件
+    require('angular-file-upload');
+    // 引人插件
+    angular.module('controllers', ['angularFileUpload']);
+
+    // 声明上传
+    var uploader = $scope.uploader = new FileUploader({
+        url: "/oprt/uploadFile",
+        queueLimit: 1// 最大上传文件数量
+        // autoUpload: true
+    });
+    uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
+        console.info('onWhenAddingFileFailed', item, filter, options);
+    };
+    uploader.onAfterAddingFile = function (fileItem) {
+        console.info('onAfterAddingFile', fileItem);
+    };
+    uploader.onAfterAddingAll = function (addedFileItems) {
+        console.info('onAfterAddingAll', addedFileItems);
+    };
+    uploader.onBeforeUploadItem = function (item) {
+        console.info('onBeforeUploadItem', item);
+    };
+    // 一个onProgressItem，一个onProgressAll 进度显示
+    uploader.onProgressItem = function (fileItem, progress) {
+        console.info('onProgressItem', fileItem, progress);
+    };
+    uploader.onProgressAll = function (progress) {
+        console.info('onProgressAll', progress);
+    };
+    uploader.onSuccessItem = function (fileItem, response, status, headers) {
+        console.info('onSuccessItem', fileItem, response, status, headers);
+    };
+    uploader.onErrorItem = function (fileItem, response, status, headers) {
+        console.info('onErrorItem', fileItem, response, status, headers);
+    };
+    uploader.onCancelItem = function (fileItem, response, status, headers) {
+        console.info('onCancelItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteItem = function (fileItem, response, status, headers) {
+        console.info('onCompleteItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteAll = function () {
+        console.info('onCompleteAll');
+    };
+
+    // 开始上传
+    $scope.uploader.uploadAll();
+
+    // 清空上传栈列
+    uploader.clearQueue();
