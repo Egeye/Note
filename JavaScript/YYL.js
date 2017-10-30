8c08e4f2038dc1da4bf8461b1c811397509eeabe
octavio <egeye4y@163.com>
2017-10-30 10:29:40 +0800

    work

---
+++ JavaScript/YYL.js
@@ -0,0 +1,12 @@
+/**
+ * 菜单左移右移点击
+ */
+function menuMoreOnclick($container,$menu) {
+    $container.children("li").unbind("click").bind("click", function() {
+        if ($(this).hasClass("admin-menu-to-right")) {
+            $menu.find(".layui-nav-item:not(.layui-hide)").last();
+        }
+    })
+}
+
+// 我想取到最后一个class为layui-nav-item或则layui-nav-item layui-this 的li，
---
+++ Python/Note.py
@@ -0,0 +1,15 @@
+# Python3 列表
+list1 = ['Google', 'xx', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+
+print("list1[0]: ", list1[3])
+print("list2[1:5]: ", list2[1:5])
+
+# Python3 元组
+
+# Python3 字典
+# 字典是另一种可变容器模型，且可存储任意类型对象。
+
+
+
+
